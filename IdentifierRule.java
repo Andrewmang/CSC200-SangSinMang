6b9fc0c498930c893aeb6f44233e8c72f38d8a2d
Andrewmang <sangsinmang97@gmail.com>
2025-09-07 18:28:33 +0800

    Update IdentifierRule.java

--- IdentifierRule.java
+++ IdentifierRule.java
@@ -1,1 +1,58 @@
+/**********************************************************************
+  
+  Program Title : Demonstrating Identifier Rules in Java
+  File Name: "IdentifierRules.java"
+  
+  Programmer : Sathish Sarma
+  Contact Details : sathish.sarma@newinti.edu.my
+  Date : 04 September 2025
+  
+ **********************************************************************/
+ 
+// Intentionally incorrect program to demonstrate invalid Java identifiers.
 
+//class switch{
+class SwitchClass{
+	String SwitchStatus = "On";
+	
+}
+
+public class IdentifierRules {
+	
+    //public static void 1start() { 
+    public static void start1 () { 
+		System.out.println("Static Method Output");
+	}
+
+    public static void main(String[] args) {
+
+        //int 2ndPlace = 2;
+        int secondplace = 2;
+
+        //int first name = 10;
+        int firstname = 10;
+
+        //int price# = 99;
+        int price = 99;
+        
+		//int user-name = 25;
+		int username = 25;
+
+		//int class = 1;
+		int classnumber = 1;
+		
+		//int double = 3;
+		int doublevalue = 3;
+		
+        //int Score = 10;
+        int score = 10;
+        System.out.println(score);
+		
+		System.out.println("This program is executed after all errors are rectified.");
+
+
+        //1start();
+        start1();
+    }
+
+}
